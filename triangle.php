c27b5f14fc3083a3f4e520d408e7dea139c76d23
Олег <bonbarcovoleg55@gmail.com>
2025-04-08 19:38:00 +0300

    init

--- triangle.php
+++ triangle.php
@@ -1,50 +1,61 @@
 <?php
-
-/**
- * Строит равнобедренный треугольник из чисел, если возможно.
- *
- * Принимает количество элементов и проверяет, можно ли разложить его
- * в треугольник вида: 1, затем 3, затем 5 и т.д. (нечётные уровни).
- * Если возможно — выводит треугольник, иначе сообщает об ошибке.
- *
- * @param int $n Количество элементов
- * @return void
- */
-
 function build_triangle($n) {
     $level = 1;
     $total_count = 0;
 
-    // Проверяем, можно ли разложить число на уровни с нечётным числом элементов
     while (true) {
         $total_count += 2 * $level - 1;
         if ($total_count == $n) {
             break;
         } elseif ($total_count > $n) {
-            echo "Невозможно построить треугольник\n";
-            return;
+            return "Невозможно построить треугольник";
         }
         $level++;
     }
 
     $current_number = 1;
+    $output = "";
+
     for ($i = 1; $i <= $level; $i++) {
         $numbers_in_row = 2 * $i - 1;
         $spaces = str_repeat(" ", ($level - $i) * 2);
+        $output .= $spaces;
 
-        echo $spaces;
         for ($j = 0; $j < $numbers_in_row; $j++) {
-            echo $current_number . " ";
+            $output .= $current_number . " ";
             $current_number++;
         }
-        echo "\n";
-    }
-}
 
-// Чтение из командной строки
-if (isset($argv[1]) && is_numeric($argv[1])) {
-    $input_number = intval($argv[1]);
-    build_triangle($input_number);
-} else {
-    echo "Пожалуйста, передайте целое число в качестве аргумента.\n";
+        $output .= "\n";
+    }
+
+    return $output;
 }
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Результат</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h2>Результат:</h2>
+
+<div class="result">
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["number"])) {
+    $n = intval($_POST["number"]);
+    echo htmlspecialchars(build_triangle($n));
+} else {
+    echo "Пожалуйста, введите число.";
+}
+?>
+</div>
+
+<p><a href="index.html">Назад</a></p>
+
+</body>
+</html>
